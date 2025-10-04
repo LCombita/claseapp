8651483a93a0652852d7fd5453b091545ec9c508
DiegoPuerta <puertadiego@gmail.com>
2025-10-04 16:11:05 -0500

    modifico el test.php

--- vulnerabilities/sqli/test.php
+++ vulnerabilities/sqli/test.php
@@ -2,13 +2,11 @@
 $host = "192.168.0.7";
 $username = "dvwa";
 $password = "password";
-
 mssql_connect($host, $username, $password);
 mssql_select_db($database);
-
-$query ="SELECT * FROM users";
-$result =mssql_query($query);
-while ( $record = mssql_fetch_array($result) ) {
-	echo $record["first_name"] .", ". $record["password"] ."<br />";
+$query = "SELECT * FROM users";
+$result = mssql_query($query);
+while ($record = mssql_fetch_array($result)) {
+    echo htmlspecialchars($record["first_name"], ENT_QUOTES, 'UTF-8') . ", " . htmlspecialchars($record["password"], ENT_QUOTES, 'UTF-8') . "<br />";
 }
 ?>
